9a78eb600e06f2e34cdeb13659dbc930e380595a
Asadullah2412 <asadullahmohammad2412@gmail.com>
2026-06-17 18:21:29 +0300

    added location methods

--- model/place.py
+++ model/place.py
@@ -1,60 +1,96 @@
-class location:
+import random
 
-    def __init__(self,clue,intelligence,fear,hunger,name):
+class Location:
 
-        self.name =name
-        self.clues = clue
+    def __init__(self,clues,intelligence,danger,hunger,name):
+
+        self.name = name
+        self.clues = clues
         self.intelligence = intelligence
-        self.fear = fear
-        self.hunger =hunger
+        self.danger = danger
+        self.hunger = hunger
+        self.neighbors = []
     
     def __repr__(self):
 
         return self.name
 
         
-    def speak(self):
+    def show_location_info(self,location):
 
-        print(f'created {self.name} ')
+        print(">>>>>>>Location Info <<<<<<<<<<<<<<<<")
+        print(f'Location name : {location.name}')
+        print(f'Clues : {location.clues}')
+        print(f'Intelligence : {location.intelligence}')
+        print(f'Fear : {location.fear}')
+        print(f'Hunger : {location.hunger}')
+
+
+
+
+    def get_random_clue(self):
+
+        return random.choice(self.clues)
+    
+
+    def add_neighbors(self,new_neighbors):
+
+        self.neighbors.extend(new_neighbors)
+        print(f'{self.name} has these neighbors {self.neighbors}')
+
+    def get_neighbors(self):
+        print(f"Neighbors are {self.neighbors}")
+
+    def is_dangerous(self):
+        if self.danger >=60:
+            print('>>>>>>> Danger Ahead <<<<<<<')
+        else:
+            print('>>>>>>>>> Not Dangerous <<<<<<')
+    
+
+
+
 
 
 
 
 
 # initializaiton of places
-church = location(clue=['A broken stained glass window showing a symbol pointing underground',
+church = Location(clues=['A broken stained glass window showing a symbol pointing underground',
 'A journal page: They come after sundown, never before the bell stops'
 'Carved into wood: coordinates scratched violently'
-'Strange static on an old radio buried near the altar'],intelligence=10,fear=-10,hunger=-20,name='church')
+'Strange static on an old radio buried near the altar'],intelligence=10,danger=75,hunger=-20,name='church')
 
 
-diner = location(clue=['A overheard conversation: someone escaped… but returned changed',
+diner = Location(clues=['A overheard conversation: someone escaped… but returned changed',
 'Receipt with hidden coded message on the back',
 'Local map with a route circled multiple times',
-'Waitress mentions: Dont trust anyone who knows too much'],intelligence=10,hunger=80,name='diner',fear=10)
+'Waitress mentions: Dont trust anyone who knows too much'],intelligence=10,hunger=80,name='diner',danger=50)
 
-colony_house = location(clue=['Old resident list with names crossed out mysteriously',
-'Basement note: ‘We tried the tunnel. Only 3 came back.',
+colony_house = Location(clues=['Old resident list with names crossed out mysteriously',
+'Basement note: We tried the tunnel. Only 3 came back.',
 'Hidden wall panel with carved tally marks (days trapped)',
-'Blueprint of town layout with missing exit route'],intelligence=5,hunger=-40,name='colony house',fear=10)
+'Blueprint of town layout with missing exit route'],intelligence=5,hunger=-40,name='colony house',danger=80)
 
 
-barn =  location(clue=['Animal behavior log showing livestock panic before disappearances',
+barn =  Location(clues=['Animal behavior log showing livestock panic before disappearances',
 'Strange claw marks too large for any known animal',
 'Hanging lantern that flickers when someone lies nearby',
-'Hidden floorboards revealing buried metal hatch'],intelligence=5,hunger=-20,name='barn',fear=10)
+'Hidden floorboards revealing buried metal hatch'],intelligence=5,hunger=-20,name='barn',danger=90)
 
 
-hospital = location(clue=['Patient log mentioning non-human night visitors',
+hospital = Location(clues=['Patient log mentioning non-human night visitors',
 'Emergency radio frequency scribbled on a clipboard',
 'Locked room with scratch marks from inside',
-'Medical chart showing unexplained repeated injuries at night'],intelligence=10,hunger=-20,name='hospital',fear= -10)
+'Medical chart showing unexplained repeated injuries at night'],intelligence=10,hunger=-20,name='hospital',danger= 10)
 
 
-forest = location(clue=['Footprints that appear and vanish suddenly',
+forest = Location(clues=['Footprints that appear and vanish suddenly',
 'A torn map fragment with missing center section',
 'Tree bark carved with repeating warning symbol',
-'A compass that always points toward the same hidden direction'],intelligence=5,hunger=-20,name='forest',fear=40)
+'A compass that always points toward the same hidden direction'],intelligence=5,hunger=-20,name='forest',danger=100)
 
 
 locations = [church,diner,colony_house,barn,hospital,forest]
+
+
